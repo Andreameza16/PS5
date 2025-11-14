f7a468d7c6d7b4c2a1c95e0c4f70fbb322949333
Andrea Meza <yosoyandreabv@gmail.com.com>
2025-11-14 02:16:04 -0600

    changes

--- app.py
+++ app.py
@@ -620,5 +620,5 @@ def enviar_contacto():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
